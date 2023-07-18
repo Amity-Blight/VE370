c6f1bb8ebe0c26456b0d9d8587811011e338dde5
amity <weeherbs@gmail.com>
2023-07-18 19:37:48 +0800

    cache

--- Labs/Lab6/Lab6/Lab6.srcs/sources_1/new/Main_Memory.v
+++ Labs/Lab6/Lab6/Lab6.srcs/sources_1/new/Main_Memory.v
@@ -23,12 +23,12 @@
 module Main_Memory(
     input           read_write_mem,
     input   [9:0]   address_mem,
-    input   [31:0]  write_data_mem,
-    output  [31:0]  read_data_mem,
-    output          Done
+    input   [31:0]  write_data_mem[3:0],
+    output  [31:0]  read_data_mem[3:0],
+    output  reg     Done
 );
 
-    reg [7:0] main_mem[1023:0];
+    reg [7:0] main_mem[63:0][15:0]; // 64 blocks in total; 16 bytes in a block
 
     initial begin
         read_data_mem = 32'b0;
