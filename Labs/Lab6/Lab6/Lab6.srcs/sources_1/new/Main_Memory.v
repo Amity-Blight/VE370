ffebd1d526bdf69e554a531e6445322e1ba72bb3
amity <weeherbs@gmail.com>
2023-07-18 21:13:47 +0800

    temp

--- Labs/Lab6/Lab6/Lab6.srcs/sources_1/new/Main_Memory.v
+++ Labs/Lab6/Lab6/Lab6.srcs/sources_1/new/Main_Memory.v
@@ -29,6 +29,8 @@ module Main_Memory(
 );
 
     reg [7:0] main_mem[63:0][15:0]; // 64 blocks in total; 16 bytes in a block
+    reg [5:0] Index;
+    integer i, j;
 
     initial begin
         read_data_mem = 32'b0;
@@ -37,7 +39,20 @@ module Main_Memory(
 
     always @(*) begin
         Done = 0;
-        
+        if (read_write_mem == 0) begin // Read operation
+            read_data_mem[0] = main_mem[Index][3:0];
+            read_data_mem[1] = main_mem[Index][7:4];
+            read_data_mem[2] = main_mem[Index][11:8];
+            read_data_mem[3] = main_mem[Index][15:12];
+        end
+        else begin // Write operation
+            for (i = 0; i < 4; i = i+1) begin
+                main_mem[Index]
+            end
+            main_mem[Index]
+        end
     end
+
+    assign Index = address_mem[9:4];
 
 endmodule
