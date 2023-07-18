d6afdb5fbd2a4aca17391bddd1934d7fed352c8b
amity <weeherbs@gmail.com>
2023-07-18 23:09:30 +0800

    simulation

--- Labs/Lab6/Lab6/Lab6.srcs/sources_1/new/Main_Memory.v
+++ Labs/Lab6/Lab6/Lab6.srcs/sources_1/new/Main_Memory.v
@@ -29,7 +29,7 @@ module Main_Memory(
 );
 
     reg [7:0] main_mem[63:0][15:0]; // 64 blocks in total; 16 bytes in a block
-    reg [5:0] Index;
+    wire [5:0] Index;
     integer i, j;
 
     initial begin
@@ -51,22 +51,22 @@ module Main_Memory(
                             main_mem[Index][3], main_mem[Index][2], main_mem[Index][1], main_mem[Index][0]};
         end
         else begin // Writing operation
-            main__mem[Index][15] = write_data_mem[127:120];
-            main__mem[Index][14] = write_data_mem[119:112];
-            main__mem[Index][13] = write_data_mem[111:104];
-            main__mem[Index][12] = write_data_mem[103:96];
-            main__mem[Index][11] = write_data_mem[95:88];
-            main__mem[Index][10] = write_data_mem[87:80];
-            main__mem[Index][9] = write_data_mem[79:72];
-            main__mem[Index][8] = write_data_mem[71:64];
-            main__mem[Index][7] = write_data_mem[63:56];
-            main__mem[Index][6] = write_data_mem[55:48];
-            main__mem[Index][5] = write_data_mem[47:40];
-            main__mem[Index][4] = write_data_mem[39:32];
-            main__mem[Index][3] = write_data_mem[31:24];
-            main__mem[Index][2] = write_data_mem[23:16];
-            main__mem[Index][1] = write_data_mem[15:8];
-            main__mem[Index][0] = write_data_mem[7:0];
+            main_mem[Index][15] = write_data_mem[127:120];
+            main_mem[Index][14] = write_data_mem[119:112];
+            main_mem[Index][13] = write_data_mem[111:104];
+            main_mem[Index][12] = write_data_mem[103:96];
+            main_mem[Index][11] = write_data_mem[95:88];
+            main_mem[Index][10] = write_data_mem[87:80];
+            main_mem[Index][9] = write_data_mem[79:72];
+            main_mem[Index][8] = write_data_mem[71:64];
+            main_mem[Index][7] = write_data_mem[63:56];
+            main_mem[Index][6] = write_data_mem[55:48];
+            main_mem[Index][5] = write_data_mem[47:40];
+            main_mem[Index][4] = write_data_mem[39:32];
+            main_mem[Index][3] = write_data_mem[31:24];
+            main_mem[Index][2] = write_data_mem[23:16];
+            main_mem[Index][1] = write_data_mem[15:8];
+            main_mem[Index][0] = write_data_mem[7:0];
         end
         Done = 1;
     end
