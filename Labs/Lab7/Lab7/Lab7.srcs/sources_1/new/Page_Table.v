988732f1e972d63847c8391ff76c70794b16dd38
amity <weeherbs@gmail.com>
2023-07-25 16:24:06 +0800

    Lab7

---
+++ Labs/Lab7/Lab7/Lab7.srcs/sources_1/new/Page_Table.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/07/25 16:17:31
+// Design Name: 
+// Module Name: Page_Table
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Page_Table(
+
+    );
+endmodule
